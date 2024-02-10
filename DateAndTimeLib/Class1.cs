57c6654b70228c7b9162793eb50c3325e41268cc
sk3l3tonis <kostik63737@gmail.com>
2024-02-10 10:57:59 +0300

    Добавьте файлы проекта.

---
+++ DateAndTimeLib/Class1.cs
@@ -0,0 +1,39 @@
+﻿namespace DateAndTimeLib
+{
+    public static class DateTimeUtils
+    {
+        public static TimeSpan GetDateDifference(DateTime firstDate, DateTime secondDate)
+        {
+            return secondDate - firstDate;
+        }
+
+        public static bool IsLeapYear(int year)
+        {
+            return DateTime.IsLeapYear(year);
+        }
+
+        public static string GetTimeOfDay(DateTime time)
+        {
+            if (time.TimeOfDay < TimeSpan.FromHours(6))
+            {
+                return "Ночь";
+            }
+            else if (time.TimeOfDay < TimeSpan.FromHours(12))
+            {
+                return "Утро";
+            }
+            else if (time.TimeOfDay < TimeSpan.FromHours(18))
+            {
+                return "День";
+            }
+            else
+            {
+                return "Вечер";
+            }
+        }
+        public static string FormatDateTime(DateTime dateTime, string format)
+        {
+            return dateTime.ToString(format);
+        }   
+    }
+}
